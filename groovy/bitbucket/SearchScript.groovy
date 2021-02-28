bda513b4c2829e6b8400ad23659d053b99f2dc70
Kim Daniel Engebretsen <kim@conduct.no>
2021-02-28 18:08:28 +0100

    Rewrote BB search script for circumvent FR queryFilter

--- groovy/bitbucket/SearchScript.groovy
+++ groovy/bitbucket/SearchScript.groovy
@@ -37,6 +37,8 @@ import org.identityconnectors.framework.common.objects.Uid
 import org.identityconnectors.common.StringUtil
 import org.identityconnectors.framework.common.objects.filter.EqualsFilter
 import org.identityconnectors.framework.common.objects.filter.Filter
+import org.identityconnectors.framework.common.objects.filter.FilterTranslator;
+import org.identityconnectors.framework.common.objects.filter.FilterVisitor;
 import org.forgerock.openidm.core.IdentityServer
 
 import static groovyx.net.http.Method.GET
@@ -57,97 +59,131 @@ def resultHandler = handler
 def identityServer = IdentityServer.getInstance()
 
 def apiVersion = identityServer.getProperty("RESTcon.apiVersion", "1.0");
+def restApi = "/rest/api/${apiVersion}"
 
 log.info("Entering RESTcon " + operation + " Script")
 
+def totalPagedResults = -1, remainingPagedResults = -1
+def pagedResultsCookie = options.pagedResultsCookie
 def query = [:]
-def queryFilter = 'true'
-//def attr = null
-//def attr_value = null
-//if (filter != null) {
-//    queryFilter = filter.accept(CRESTFilterVisitor.VISITOR, [
-//            translateName: { String name ->
-//                if (AttributeUtil.namesEqual(name, Uid.NAME)) {
-//                    attr = "name"
-//                    return "id"
-//                } else if (AttributeUtil.namesEqual(name, Name.NAME)) {
-//                    attr = "name"
-//                    return "key"
-//                } else {
-//                    attr = name
-//                    return name
-//                    //throw new IllegalArgumentException("Unknown field name: ${name}");
-//                }
-//            },
-//            convertValue : { Attribute value ->
-//                if (AttributeUtil.namesEqual(value.name, "members")) {
-//                    return value.value
-//                } else {
-//                    attr_value = AttributeUtil.getStringValue(value)
-//                    return AttributeUtil.getStringValue(value)
-//                }
-//            }] as VisitorParameter).toString();
-//}
-//
-query['_queryFilter'] = queryFilter
-////if (null != attr && null != attr_value) {
-////    query[attr] = attr_value
-////}
+def queryFilter = ''
+
+if (filter != null) {
+    queryFilter = filter.accept(new FilterTranslator(), [
+            translateName: { String name ->
+                if (AttributeUtil.namesEqual(name, Uid.NAME)) {
+                    return "id"
+                } else if (AttributeUtil.namesEqual(name, Name.NAME)) {
+                    return "name"
+                } else {
+                    return name
+                }
+            },
+            convertValue : { Attribute value ->
+                    return AttributeUtil.getStringValue(value)
+            }] as VisitorParameter).toString();
+}
 
 if (null != options.pageSize) {
     query['limit'] = options.pageSize
-    if (null != options.pagedResultsCookie) {
-        query['_pagedResultsCookie'] = options.pagedResultsCookie
-    }
     if (null != options.pagedResultsOffset) {
         query['start'] = options.pagedResultsOffset
     }
 }
-if (filter != null) {
-    if (filter instanceof EqualsFilter && ((EqualsFilter) filter).attribute.is(Uid.NAME)) {
-        def uid = AttributeUtil.getStringValue(((EqualsFilter) filter).attribute)
-        query['name'] = uid
-        log.info("Filter RESTcon " + query + " Script")
-    }
+
+def getOneById = { objectType, objectId ->
+	def searchResult = connection.request(GET) { req ->
+	    uri.path = "${restApi}/${objectType}/${objectId}"
+	    uri.query = query
+	
+	    log.info("RESTcon URI " + uri + " Script")
+	    response.success = { resp, json ->
+	        json.values.each() { record ->
+	            resultHandler {
+	                uid record.key
+	                id record.id.toString()
+	                
+	            	record.each() { key, value -> attribute key, value }
+	            }
+	        }
+	        json
+	    }
+	}
+    return new SearchResult("0", 0);
 }
 
-switch (objectClass.objectClassValue) {
-    case "project":
-        def searchResult = connection.request(GET) { req ->
-            uri.path = "/rest/api/${apiVersion}/projects"
-            uri.query = query
-
-            log.info("RESTcon URI " + uri + " Script")
-            response.success = { resp, json ->
-                json.values.each() { record ->
-                    resultHandler {
-                        uid record.key
-                        id record.id.toString()
-                        
-                    	record.each() { key, value -> attribute key, value }
-                    }
-                }
-                json
-            }
+def getManyById = { objectType, matches ->
+	def map = ['result':[]]
+	matches.each() { match ->
+		objectId = match[1] 
+		connection.request(GET) { req ->
+	    	uri.path = "${restApi}/${objectType}/${objectId}"
+		    response.success = { resp, json ->
+		        json.values.each() { record ->
+    	        	map['result'] += record 
+    	        }
+    	    }
+    	}
+	}
+    map.result.each() { record ->
+        resultHandler {
+            uid record.id
+            if (objectType == 'projects') { id record.key }
+            else { id record.name }
+            record.each() { key, value -> attribute key, value }
         }
-        return new SearchResult(null, -1)
-    case "repo":
-        def searchResult = connection.request(GET) { req ->
-            uri.path = "/rest/api/${apiVersion}/repos"
-            uri.query = query
+    }
+    return new SearchResult("0", 0);
+}
 
-            log.info("RESTcon " + uri + " Script")
-            response.success = { resp, json ->
-                json.values.each() { record ->
-                    resultHandler {
-                        uid record.name
-                        id record.slug
-                        
-                    	record.each() { key, value -> attribute key, value }
-                    }
-                }
-                json
-            }
-        }
-        return new SearchResult(null, -1)
+def getSearchResults = { objectType ->
+    if (objectType == "projects") {
+        queryFilter = queryFilter.replace("__NAME__", "key")
+    } else {
+        queryFilter = queryFilter.replace("__NAME__", "name")
+    }
+	def searchResult = connection.request(GET) { req ->
+	    uri.path = "${restApi}/${objectType}"
+	    uri.query = queryFilter
+	
+	    log.info("RESTcon URI path:" + uri.path + " query:"+ uri.query +" Script")
+	    response.success = { resp, json ->
+	        json.values.each() { record ->
+	            resultHandler {
+	                uid record.key
+	                id record.id.toString()
+	                
+	            	record.each() { key, value -> attribute key, value }
+	            }
+	        }
+	        json
+	    }
+	}
+    return new SearchResult("0", 0);
+}
+
+
+def doSearch = { objectType, idPattern ->
+   if ((m = queryFilter =~ "^id = ${idPattern}\$")) {
+       return getOneById(objectType, m[0][1])
+   }
+   else if ((m = queryFilter =~ "(?: OR )id = ${idPattern}")) {
+       return getManyById(objectType, m)
+   }
+   else {
+       return getSearchResults(objectType)
+   }
+}
+
+
+switch (objectClass) {
+    case 'ObjectClass: project':
+		doSearch('projects', /'([0-9]+)'/)
+		break;
+    case 'ObjectClass: repo':
+		doSearch('repos', /'([0-9]+)'/)
+		break;
+	default:
+		log.info("SEARCH Script got bad objectClass '${objectClass}'")
+		break;
 }
